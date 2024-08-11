8a21dc27403aab1800e8fc840afbeb081b5a55ad
Valentin679 <67257342+Valentin679@users.noreply.github.com>
2024-08-11 20:54:47 +0300

    new mongo

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
+const {MongoClient} = require("mongodb");
+// const Schema = mongoose.Schema;
+const objectId = require("mongodb").ObjectId;
+const client = new MongoClient("mongodb+srv://admin:12345asd@rat.gk7dz4o.mongodb.net/?appName=rat");
+
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/api/users", async (req, res)=>{
+    // получаем всех пользователей
+    const db = client.db("Menu");
+    const collectionNav = db.collection("nav");
+    const navList = await collectionNav.find().toArray();
+    res.send(navList);
+});
+app.listen(8800, () => console.log("Server ready on port 3000."));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
