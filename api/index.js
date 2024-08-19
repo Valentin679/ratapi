6e68c9f982fb4996f5bbb35f9f6b7ed30694fad6
Valentin679 <67257342+Valentin679@users.noreply.github.com>
2024-08-19 20:06:43 +0300

    cat

--- api/index.js
+++ api/index.js
@@ -14,6 +14,24 @@ app.get("/api/users", async (req, res)=>{
     const navList = await collectionNav.find().toArray();
     res.send(navList);
 });
+app.get("/api/categories", async (req, res)=>{
+    // получаем всех пользователей
+    const db = client.db("material");
+    const collectionCategories = db.collection("materials_categories");
+    const catList = await collectionCategories.find().toArray();
+    res.send(catList);
+});
+app.post("/api/categories", async (req, res) =>{
+
+    if(!req.body) return res.sendStatus(400);
+
+    const userName = req.body.name;
+    const userAge = req.body.age;
+    const user = new User({name: userName, age: userAge});
+    // сохраняем в бд
+    await user.save();
+    res.send(user);
+});
 app.listen(8800, () => console.log("Server ready on port 3000."));
 
 
